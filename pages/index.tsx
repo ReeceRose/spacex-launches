bc3a7f45aee91539e0ec1fccf90e7d4092b89faf
Reece Rose <reecerose18@gmail.com>
2021-02-07 19:41:01 -0500

    Initial project setup

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>SpaceX Launches</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+}
